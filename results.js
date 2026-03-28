13baa19466efd8e9b660a384365c88d54c02221d
Himadri Goswami <himadrigoswami2606@gmail.com>
2026-03-28 21:07:01 +0530

    Add files via upload

---
+++ results.js
@@ -0,0 +1,21 @@
+window.MATCH_RESULTS = {
+  /*
+    Daily update guide:
+
+    1. Pick the match id from script.js, like "m1" or "m27".
+    2. Add the finished result here.
+    3. Save and refresh index.html.
+
+    Format:
+    m1: {
+      completedWinner: "RCB", // or "SRH" or "NR"
+      scoreboard: {
+        RCB: { score: "174/6", overs: "20.0" },
+        SRH: { score: "168/8", overs: "20.0" },
+      },
+    },
+
+    Overs can be written like "20.0" or "18.3".
+    Score should be written like "150/7".
+  */
+};

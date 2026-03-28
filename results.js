114602d010a916f72749cc02b7ba2dad7b2b2785
Himadri Goswami <himadrigoswami2606@gmail.com>
2026-03-28 21:13:57 +0530

    Update results.js

--- results.js
+++ results.js
@@ -1,21 +1,9 @@
 window.MATCH_RESULTS = {
-  /*
-    Daily update guide:
-
-    1. Pick the match id from script.js, like "m1" or "m27".
-    2. Add the finished result here.
-    3. Save and refresh index.html.
-
-    Format:
-    m1: {
-      completedWinner: "RCB", // or "SRH" or "NR"
-      scoreboard: {
-        RCB: { score: "174/6", overs: "20.0" },
-        SRH: { score: "168/8", overs: "20.0" },
-      },
+  m1: {
+    completedWinner: "RCB",
+    scoreboard: {
+      RCB: { score: "182/6", overs: "20.0" },
+      SRH: { score: "176/8", overs: "20.0" },
     },
-
-    Overs can be written like "20.0" or "18.3".
-    Score should be written like "150/7".
-  */
-};
+  },
+}

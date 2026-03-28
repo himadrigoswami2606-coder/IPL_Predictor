5b3a1d9ea69c31eea2a9e155738314d02ce540c0
Himadri Goswami <himadrigoswami2606@gmail.com>
2026-03-28 21:33:38 +0530

    Update results.js

--- results.js
+++ results.js
@@ -1,9 +1,3 @@
 window.MATCH_RESULTS = {
-  m1: {
-    completedWinner: "RCB",
-    scoreboard: {
-      RCB: { score: "182/6", overs: "20.0" },
-      SRH: { score: "176/8", overs: "20.0" },
-    },
-  },
+  
 }
